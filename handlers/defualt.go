87ee4ffb87df753df92b700fb3b602be102f6edc
Sigurd Riseth <sigurd.riseth@gmail.com>
2025-02-19 07:11:37 +0100

    Refactored default endpoint

--- handlers/defualt.go
+++ handlers/defualt.go
@@ -11,10 +11,19 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNotFound)
 
+	// Add a list of endpoints and how to call them
+	endpoints := []string{
+		"The following endpoints are available:",
+		"GET /countryinfo/v1/info/{countryCode} - Get information about a country. Example: /countryinfo/v1/info/NO",
+		"GET /countryinfo/v1/population/{countryCode} - Get population data. Example: /countryinfo/v1/population/US",
+		"GET /countryinfo/v1/status - Check the service status.",
+	}
+
 	// Create error response
-	response := utils.ErrorResponse{
-		Error:   http.StatusNotFound,
+	response := utils.APIResponse{
+		Error:   true,
 		Message: "You seem lost. The requested page was not found.",
+		Data:    endpoints,
 	}
 
 	// Encode response as JSON
