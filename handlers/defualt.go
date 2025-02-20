d1fa5d771cb99ff75458ac4f08472bd604bb0549
Sigurd Riseth <sigurd.riseth@gmail.com>
2025-02-20 10:50:26 +0100

    Merge

--- handlers/defualt.go
+++ handlers/defualt.go
@@ -11,12 +11,14 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNotFound)
 
-	// Add a list of endpoints and how to call them
+	// Add a list of endpoints and how to call them, including query parameters
 	endpoints := []string{
 		"The following endpoints are available:",
-		"GET /countryinfo/v1/info/{countryCode} - Get information about a country. Example: /countryinfo/v1/info/NO",
-		"GET /countryinfo/v1/population/{countryCode} - Get population data. Example: /countryinfo/v1/population/US",
-		"GET /countryinfo/v1/status - Check the service status.",
+		"GET /country/v1/info/{countryCode} - Get information about a country. Example: /country/v1/info/NO",
+		"  Optional query parameter: ?limit=x (limits number of cities). Example: /country/v1/info/NO?limit=5",
+		"GET /country/v1/population/{countryCode} - Get population data. Example: /country/v1/population/US",
+		"  Optional query parameter: ?limit=YYYY-YYYY (limits data to a year range). Example: /country/v1/population/US?limit=2002-2008",
+		"GET /country/v1/status - Check the service status.",
 	}
 
 	// Create error response
